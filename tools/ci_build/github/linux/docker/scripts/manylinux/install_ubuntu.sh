09219128d265c46d2c416a29a328a036958da7c8
Yi Zhang <zhanyi@microsoft.com>
2022-06-09 10:09:42 -0700

    minor fix

--- tools/ci_build/github/linux/docker/scripts/manylinux/install_ubuntu.sh
+++ tools/ci_build/github/linux/docker/scripts/manylinux/install_ubuntu.sh
@@ -6,7 +6,7 @@ os_major_version=$(cat /etc/lsb-release | tr -dc '0-9.'|cut -d \. -f1)
 apt-get update -y
 
 echo "installing for os major version : $os_major_version"
-apt-get install -y gdb expat-devel build-essential tar unzip zlib-devel make libunwind  aria2  bzip2 bzip2-devel
+apt-get install -y gdb  build-essential tar unzip  make  aria2  bzip2
 
 # Install Java
 # Install automatic documentation generation dependencies
